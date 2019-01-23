77a4f83427035e3678df5a54f520f86c92ca513d
Atul <atulpdeshpande@gmail.com>
2019-01-23 17:19:59 -0600

    Reordered param assignment. Brought out gene_influence output in example.

--- SCINGE_Example.m
+++ SCINGE_Example.m
@@ -1,4 +1,8 @@
+%% Simlpe example that runs SCINGE for two replicates of two hyperparameter settings
+
 addpath(genpath('.'));
+
+%% Generate list of parameter combinations
 param_list{1}.ID = 541;
 param_list{1}.lambda = 0.01;
 param_list{1}.dT = 10;
@@ -8,20 +12,21 @@ param_list{1}.prob_zero_removal = 0;
 param_list{1}.prob_remove_samples = 0.2;
 param_list{1}.family = 'gaussian';
 
-
+param_list{2}.ID = 542;
 param_list{2}.lambda = 0.01;
 param_list{2}.dT = 5;
 param_list{2}.num_lags = 9;
 param_list{2}.kernel_width = 4;
 param_list{2}.prob_zero_removal = 0.2;
-param_list{2}.family = 'gaussian';
 param_list{2}.prob_remove_samples = 0.1;
-param_list{2}.ID = 542;
+param_list{2}.family = 'gaussian';
 
+%% Specify Path to Input data and path to Output folder, gene_list and number of subsampled replicates
 Data = 'data1/X_SCODE_data.mat';
 outdir = 'Output';
 num_replicates = 2;
 load('data1/tf.mat');
 gene_list = tf;
 
-ranked_edges = SCINGE(gene_list,Data,outdir,num_replicates,param_list);
+%% Run SCINGE
+[ranked_edges, gene_influence] = SCINGE(gene_list,Data,outdir,num_replicates,param_list);
