ec4e2772118769c5aa35e179a9e8cdb34454ece6
Anthony Gitter <gitter@biostat.wisc.edu>
2019-03-19 08:58:01 -0500

    Attempt to compile MEX files with Octave

--- install_glmnet_docker.sh
+++ install_glmnet_docker.sh
@@ -3,4 +3,9 @@
 # http://web.stanford.edu/~hastie/glmnet_matlab/index.html
 # and compiling for Octave
 echo "Test install script"
-
+wget http://web.stanford.edu/~hastie/glmnet_matlab/glmnet_matlab.zip
+unzip glmnet_matlab.zip
+cd glmnet_matlab
+ls
+mkoctfile --mex glmnetMex.F GLMnet.f
+cd ..
