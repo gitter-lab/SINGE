0c9d42c54d25f7aefdf026de9d02a9fc3d996f77
Anthony Gitter <gitter@biostat.wisc.edu>
2019-03-19 10:17:04 -0500

    Debug fintrf.h error

--- install_glmnet_docker.sh
+++ install_glmnet_docker.sh
@@ -9,5 +9,6 @@ wget http://web.stanford.edu/~hastie/glmnet_matlab/glmnet_matlab.zip
 unzip glmnet_matlab.zip
 cd glmnet_matlab
 ls
+wget https://raw.githubusercontent.com/SheffieldML/GPmat/master/kern/mex/fintrf.h
 mkoctfile --mex glmnetMex.F GLMnet.f
 cd ..
