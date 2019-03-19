9bb3d8b9a7b5f639af32e7877b53c12532af1529
Anthony Gitter <gitter@biostat.wisc.edu>
2019-03-19 10:03:29 -0500

    Install wget

--- install_glmnet_docker.sh
+++ install_glmnet_docker.sh
@@ -3,6 +3,7 @@
 # http://web.stanford.edu/~hastie/glmnet_matlab/index.html
 # and compiling for Octave
 echo "Test install script"
+sudo apt-get install wget
 wget http://web.stanford.edu/~hastie/glmnet_matlab/glmnet_matlab.zip
 unzip glmnet_matlab.zip
 cd glmnet_matlab
