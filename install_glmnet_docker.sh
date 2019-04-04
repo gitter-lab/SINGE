c1ac0c45b92e4111b6b65475c5baf4b945f604e5
Anthony Gitter <agitter@users.noreply.github.com>
2019-04-04 09:22:51 -0500

    Add more debugging output

--- install_glmnet_docker.sh
+++ install_glmnet_docker.sh
@@ -2,14 +2,16 @@
 # Install Glmnet by downloading from
 # http://web.stanford.edu/~hastie/glmnet_matlab/index.html
 # and compiling for Octave
-echo "Test install script"
 apt-get update
 apt-get -y install wget
 wget http://web.stanford.edu/~hastie/glmnet_matlab/glmnet_matlab.zip
 unzip glmnet_matlab.zip
+
 cd glmnet_matlab
-ls
 wget https://raw.githubusercontent.com/SheffieldML/GPmat/master/kern/mex/fintrf.h
-mkoctfile --mex glmnetMex.F GLMnet.f
-ls
+mkoctfile --verbose --mex glmnetMex.F GLMnet.f
+
 cd ..
+
+# To help debugging
+gfortran --version
