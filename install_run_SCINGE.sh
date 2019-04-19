ad02f96efcc9169307f3d55b0e97e5e1e390dbe8
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-19 15:30:11 -0500

    Troubleshoot libxt6 problem Similar to https://www.mathworks.com/matlabcentral/answers/374823-when-starting-a-matlab-production-server-instance-why-to-do-i-receive-the-message-in-my-log-file-e

--- install_run_SCINGE.sh
+++ install_run_SCINGE.sh
@@ -3,6 +3,9 @@
 wget https://www.biostat.wisc.edu/~gitter/tmp/86e65385d9e50e21be23bd8185e02e3b/SCINGE_Example
 chmod u+x SCINGE_Example
 
+apt-get update
+apt-get -y install libxt6
+
 # Run the MATLAB-generated wrapper script
 ./run_SCINGE_Example.sh /usr/local/MATLAB/MATLAB_Runtime/v94
 
