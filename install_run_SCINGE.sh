c8570158edbdca255d890aad1c2ec6c9da8ae00b
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-19 15:21:24 -0500

    Switch to wget

--- install_run_SCINGE.sh
+++ install_run_SCINGE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install the compiled SCINGE_Example executable from a temporary location
-curl -O https://www.biostat.wisc.edu/~gitter/tmp/86e65385d9e50e21be23bd8185e02e3b/SCINGE_Example
+wget https://www.biostat.wisc.edu/~gitter/tmp/86e65385d9e50e21be23bd8185e02e3b/SCINGE_Example
 chmod u+x SCINGE_Example
 
 # Run the MATLAB-generated wrapper script
