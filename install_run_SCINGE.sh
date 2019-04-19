8c66d9f987a02d2e427f81eebeca4418bf95c3aa
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-19 14:45:49 -0500

    Attempt to run SCINGE_Example in MATLAB 2015b runtime

---
+++ install_run_SCINGE.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install the compiled SCINGE_Example executable from a temporary location
+curl -O https://www.biostat.wisc.edu/~gitter/tmp/86e65385d9e50e21be23bd8185e02e3b/SCINGE_Example
+
+# Run the MATLAB-generated wrapper script
+./run_SCINGE_Example.sh /opt/mcr
