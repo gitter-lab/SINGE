678337eac9f23c116c79803ccaff567b0d411e75
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-19 14:54:30 -0500

    Troubleshoot eval error

--- install_run_SCINGE.sh
+++ install_run_SCINGE.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Install the compiled SCINGE_Example executable from a temporary location
 curl -O https://www.biostat.wisc.edu/~gitter/tmp/86e65385d9e50e21be23bd8185e02e3b/SCINGE_Example
+ls -l
+chmod u+x SCINGE_Example
 
 # Run the MATLAB-generated wrapper script
-./run_SCINGE_Example.sh /opt/mcr
+./run_SCINGE_Example.sh /opt/mcr/v90
