cca704cab3684522a6d27ca93f241ee8ab6b39aa
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-26 18:02:54 -0500

    Create test script to run csvdiff

---
+++ tests/compare_example_output.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Compare the SCINGE_Example.m output with the reference output
+# Must have already run SCINGE_Example.m using the default output directory
+# Must have Python package csvdiff installed https://pypi.org/project/csvdiff/
+csvdiff --style=summary Gene_Name ../Output/SCINGE_Gene_Influence.txt reference/SCINGE_Gene_Influence.txt
+
