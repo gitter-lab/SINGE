3112f2ed266642836d77bbc0eae92c42358c1058
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-26 17:24:39 -0500

    Use Miniconda for Python

--- install_run_SCINGE.sh
+++ install_run_SCINGE.sh
@@ -4,11 +4,20 @@ wget --quiet https://www.biostat.wisc.edu/~gitter/tmp/369b3eb57db4fb1fce29c5ae28
 chmod u+x SCINGE_Example
 
 apt-get update
-apt-get -y install libxt6 python3-pip
+apt-get -y install libxt6
 
-python3 --version
+# Install Miniconda3 following https://hub.docker.com/r/continuumio/miniconda3/dockerfile
+wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-4.6.14-Linux-x86_64.sh -O ~/miniconda.sh
+/bin/bash ~/miniconda.sh -b -p /opt/conda
+rm ~/miniconda.sh
+/opt/conda/bin/conda clean -tipsy
+ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
+echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc
+echo "conda activate base" >> ~/.bashrc
+
+# Install csvdiff
+python --version
 pip install csvdiff
 
 # Run the MATLAB-generated wrapper script
 ./run_SCINGE_Example.sh /usr/local/MATLAB/MATLAB_Runtime/v94
-
