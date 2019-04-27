64cf7f511e18e0e73dde428c64036e413ca85ec7
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-27 07:07:22 -0500

    Use csvdiff to test gene and edge output files

--- tests/compare_example_output.sh
+++ tests/compare_example_output.sh
@@ -3,4 +3,22 @@
 # This script must be called from the directory that contains SCINGE_Example.m
 # Must have already run SCINGE_Example.m using the default output directory
 # Must have Python package csvdiff installed https://pypi.org/project/csvdiff/
-csvdiff --style=summary --sep='	' --significance=5 Gene_Name Output/SCINGE_Gene_Influence.txt tests/reference/SCINGE_Gene_Influence.txt
+echo Comparing SCINGE_Gene_Influence.txt
+csvdiff --style=summary --sep='	' --significance=5 --output=csvdiff.out Gene_Name Output/SCINGE_Gene_Influence.txt tests/reference/SCINGE_Gene_Influence.txt
+cat csvdiff.out
+comparison=$(cat csvdiff.out)
+rm csvdiff.out
+if [[ "$comparison" != 'files are identical' ]] ; then
+  exit 1
+fi
+
+# csvdiff requires a unique key in each row
+# Provide the Regulator and Target columns together as the index
+echo Comparing SCINGE_Ranked_Edge_List.txt
+csvdiff --style=summary --sep='	' --significance=5 --output=csvdiff.out Regulator,Target Output/SCINGE_Ranked_Edge_List.txt tests/reference/SCINGE_Ranked_Edge_List.txt
+cat csvdiff.out
+comparison=$(cat csvdiff.out)
+rm csvdiff.out
+if [[ "$comparison" != 'files are identical' ]] ; then
+  exit 1
+fi
