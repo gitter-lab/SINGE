13ab2bc48cd51a17d229e706b936a1dc050cf4d0
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-27 06:18:36 -0500

    Fix test script directories

--- tests/compare_example_output.sh
+++ tests/compare_example_output.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Compare the SCINGE_Example.m output with the reference output
+# This script must be called from the directory that contains SCINGE_Example.m
 # Must have already run SCINGE_Example.m using the default output directory
 # Must have Python package csvdiff installed https://pypi.org/project/csvdiff/
-csvdiff --style=summary --sep='	' --significance=5 Gene_Name ../Output/SCINGE_Gene_Influence.txt reference/SCINGE_Gene_Influence.txt
+csvdiff --style=summary --sep='	' --significance=5 Gene_Name Output/SCINGE_Gene_Influence.txt tests/reference/SCINGE_Gene_Influence.txt
