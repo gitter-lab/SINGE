c787f472360ce788e74e254a389543028134b580
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-27 06:04:14 -0500

    Fix csvdiff separator and check 5 significant figures

--- tests/compare_example_output.sh
+++ tests/compare_example_output.sh
@@ -2,4 +2,4 @@
 # Compare the SCINGE_Example.m output with the reference output
 # Must have already run SCINGE_Example.m using the default output directory
 # Must have Python package csvdiff installed https://pypi.org/project/csvdiff/
-csvdiff --style=summary Gene_Name ../Output/SCINGE_Gene_Influence.txt reference/SCINGE_Gene_Influence.txt
+csvdiff --style=summary --sep='	' --significance=5 Gene_Name ../Output/SCINGE_Gene_Influence.txt reference/SCINGE_Gene_Influence.txt
