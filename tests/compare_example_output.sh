09e30d8678490e4450fad85849a5cd1c5abed2b9
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-27 05:56:49 -0500

    Troubleshoot environment

--- tests/compare_example_output.sh
+++ tests/compare_example_output.sh
@@ -3,4 +3,3 @@
 # Must have already run SCINGE_Example.m using the default output directory
 # Must have Python package csvdiff installed https://pypi.org/project/csvdiff/
 csvdiff --style=summary Gene_Name ../Output/SCINGE_Gene_Influence.txt reference/SCINGE_Gene_Influence.txt
-
