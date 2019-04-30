642fd353e74327a697468f67c2757e5437e8c34a
Anthony Gitter <gitter@biostat.wisc.edu>
2019-04-30 13:11:48 -0500

    Compare sparse adjacency matrices in test script

--- tests/compare_example_output.sh
+++ tests/compare_example_output.sh
@@ -5,8 +5,11 @@
 # Must run inside the conda environment specified by environment.yml or have
 # those Python packages available
 
+outdir=Output
+refdir=tests/reference
+
 echo Comparing SCINGE_Gene_Influence.txt
-csvdiff --style=summary --sep='	' --significance=5 --output=csvdiff.out Gene_Name Output/SCINGE_Gene_Influence.txt tests/reference/SCINGE_Gene_Influence.txt
+csvdiff --style=summary --sep='	' --significance=5 --output=csvdiff.out Gene_Name $outdir/SCINGE_Gene_Influence.txt $refdir/SCINGE_Gene_Influence.txt
 cat csvdiff.out
 comparison=$(cat csvdiff.out)
 rm csvdiff.out
@@ -17,10 +20,20 @@ fi
 # csvdiff requires a unique key in each row
 # Provide the Regulator and Target columns together as the index
 echo Comparing SCINGE_Ranked_Edge_List.txt
-csvdiff --style=summary --sep='	' --significance=5 --output=csvdiff.out Regulator,Target Output/SCINGE_Ranked_Edge_List.txt tests/reference/SCINGE_Ranked_Edge_List.txt
+csvdiff --style=summary --sep='	' --significance=5 --output=csvdiff.out Regulator,Target $outdir/SCINGE_Ranked_Edge_List.txt $refdir/SCINGE_Ranked_Edge_List.txt
 cat csvdiff.out
 comparison=$(cat csvdiff.out)
 rm csvdiff.out
 if [[ "$comparison" != 'files are identical' ]] ; then
   exit 1
 fi
+
+echo Comparing sparse adjacency matrices
+for id in 541 542
+do
+  for rep in 1 2
+  do
+    filename=AdjMatrix_data1_X_SCODE_datapmat_ID_$id_replicate_$rep.mat
+    python compare_adj_matrices.py $outdir/$filename $refdir/$filename
+  done
+done
