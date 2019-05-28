8f32ca4e617b99c177872a9a29c4f4fc62168e5d
Atul <atulpdeshpande@gmail.com>
2019-05-28 09:57:15 -0500

    Changed SCINGE references in SINGE_Example and SINGE.m

--- SINGE_Example.m
+++ SINGE_Example.m
@@ -1,4 +1,4 @@
-%% Simple example that runs SCINGE for two replicates of two hyperparameter settings
+%% Simple example that runs SINGE for two replicates of two hyperparameter settings
 clear all;
 close all;
 clc;
--- code/SINGE.m
+++ code/SINGE.m
@@ -1,17 +1,17 @@
-function [ranked_edges, gene_influence] = SCINGE(gene_list,Data,outdir,num_replicates,param_list)
-% [ranked_edges, gene_influence] = SCINGE(gene_list,Data,outdir,num_replicates,param_list)
-% Standalone SCINGE implementation. 
+function [ranked_edges, gene_influence] = SINGE(gene_list,Data,outdir,num_replicates,param_list)
+% [ranked_edges, gene_influence] = SINGE(gene_list,Data,outdir,num_replicates,param_list)
+% Standalone SINGE implementation. 
 % Inputs:
 % gene_list = N x 1 cell array with list of relevant genes in the data set
 % Data = string representing the path of mat file containing the expression
 % data corresponding to above gene_list in the form of cell array X.
 % outdir = directory path to store individual GLG test results before Borda
 % aggregation
-% num_replicates = number of subsampled replicates (global SCINGE parameter)
+% num_replicates = number of subsampled replicates (global SINGE parameter)
 % param_list = list of hyperparameter combinations for individual GLG tests
 % Outputs:
-% ranked_edges = ranked list of gene interactions with corresponding SCINGE scores
-% gene_influence = ranked lists of regulators (genes) with corresponding SCINGE influence
+% ranked_edges = ranked list of gene interactions with corresponding SINGE scores
+% gene_influence = ranked lists of regulators (genes) with corresponding SINGE influence
 SINGE_version = '0.1.0';
 display(SINGE_version);
 for rep = 1:num_replicates
@@ -35,8 +35,8 @@ ranked_edges = adjmatrix2edgelist(Agg,gene_list);
 gene_influence = [cell2table(gene_list(ind)) array2table(influence)];
 gene_influence.Properties.VariableNames{1} = 'Gene_Name';
 ranked_edgesw = ranked_edges;
-ranked_edgesw.SCINGE_Score = floor(ranked_edgesw.SCINGE_Score*10^5)/10^5;
-gene_influencew = gene_influence;
+ranked_edgesw.SINGE_Score = floor(ranked_edgesw.SINGE_Score*10^5)/10^5;
+gene_influencew = gene_influence;git add code
 gene_influencew.influence = floor(gene_influencew.influence*10^5)/10^5;
-writetable(ranked_edgesw,fullfile(outdir,'SCINGE_Ranked_Edge_List.txt'),'WriteVariableNames',true,'WriteRowNames',false,'Delimiter','\t');
-writetable(gene_influencew,fullfile(outdir,'SCINGE_Gene_Influence.txt'),'WriteVariableNames',true,'WriteRowNames',false,'Delimiter','\t');
+writetable(ranked_edgesw,fullfile(outdir,'SINGE_Ranked_Edge_List.txt'),'WriteVariableNames',true,'WriteRowNames',false,'Delimiter','\t');
+writetable(gene_influencew,fullfile(outdir,'SINGE_Gene_Influence.txt'),'WriteVariableNames',true,'WriteRowNames',false,'Delimiter','\t');
