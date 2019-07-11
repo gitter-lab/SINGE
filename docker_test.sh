7750ffeb3e3cd63025ac1b3e1d8258a896d58306
Anthony Gitter <gitter@biostat.wisc.edu>
2019-07-11 13:54:52 -0500

    Move docker run commands to test script

---
+++ docker_test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Run SINGE and tests inside the Docker image
+# Move the binary and check the version
+mv /download/SCINGE_Example .
+md5sum SCINGE_Example
+
+# Run SINGE on the example data and inspect the output
+./run_SCINGE_Example.sh /usr/local/MATLAB/MATLAB_Runtime/v94
+ls Output/ -l
+
+# Set up the conda test environment
+source ~/.bashrc
+conda activate scinge-test
+conda list
+
+# Run the tests
+tests/compare_example_output.sh
