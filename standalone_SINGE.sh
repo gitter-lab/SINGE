d41ca312b1c1dadbda06083665183c392f976c44
Anthony Gitter <gitter@biostat.wisc.edu>
2019-08-21 06:30:42 -0500

    Add full MATLAB runtime path

--- standalone_SINGE.sh
+++ standalone_SINGE.sh
@@ -4,7 +4,7 @@ gene_list=$2
 outdir=$3
 hypefile=$4
 while read arg||[ -n "$arg" ]; do 
-    ./run_SINGE_GLG_Test.sh v94 $data -outdir $outdir $arg
+    ./run_SINGE_GLG_Test.sh /usr/local/MATLAB/MATLAB_Runtime/v94 $data -outdir $outdir $arg
 done < $hypefile
 
-./run_SINGE_Aggregate.sh v94 $gene_list $data $outdir
+./run_SINGE_Aggregate.sh /usr/local/MATLAB/MATLAB_Runtime/v94 $gene_list $data $outdir
