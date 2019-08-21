4c42375157729066fb0f4e57fa71c2a5c9890a6a
Anthony Gitter <gitter@biostat.wisc.edu>
2019-08-21 05:39:50 -0500

    Restore and rename SCINGE_Example.m We still would like an example of how to run SINGE directly through MATLAB

---
+++ SINGE_Example.m
@@ -0,0 +1,38 @@
+%% Simple example that runs SINGE for two replicates of two hyperparameter settings
+clear all;
+close all;
+clc;
+if ~isdeployed
+    addpath(genpath('.'));
+end
+
+%% Generate list of parameter combinations
+param_list{1}.ID = 541;
+param_list{1}.lambda = 0.01;
+param_list{1}.dT = 10;
+param_list{1}.num_lags = 5;
+param_list{1}.kernel_width = 2;
+param_list{1}.prob_zero_removal = 0;
+param_list{1}.prob_remove_samples = 0.2;
+param_list{1}.family = 'gaussian';
+param_list{1}.date = '01/31/2019';
+
+param_list{2}.ID = 542;
+param_list{2}.lambda = 0.01;
+param_list{2}.dT = 5;
+param_list{2}.num_lags = 9;
+param_list{2}.kernel_width = 4;
+param_list{2}.prob_zero_removal = 0.2;
+param_list{2}.prob_remove_samples = 0.1;
+param_list{2}.family = 'gaussian';
+param_list{2}.date = '31-Jan-2019'; % An alternative supported date format
+
+%% Specify Path to Input data and path to Output folder, gene_list and number of subsampled replicates
+Data = 'data1/X_SCODE_data.mat';
+outdir = 'Output';
+num_replicates = 2;
+load('data1/tf.mat');
+gene_list = tf;
+
+%% Run SINGE
+[ranked_edges, gene_influence] = SINGE(gene_list,Data,outdir,num_replicates,param_list);
