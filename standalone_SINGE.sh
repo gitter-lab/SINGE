33960a27af81084e41480dfed84da08f453daf08
Anthony Gitter <gitter@biostat.wisc.edu>
2019-08-23 05:53:38 -0500

    Take MATLAB runtime path as argument

--- standalone_SINGE.sh
+++ standalone_SINGE.sh
@@ -3,8 +3,9 @@ data=$1
 gene_list=$2
 outdir=$3
 hypefile=$4
+runtime=$5
 while read arg||[ -n "$arg" ]; do 
-    ./run_SINGE_GLG_Test.sh /usr/local/MATLAB/MATLAB_Runtime/v94 $data -outdir $outdir $arg
+    ./run_SINGE_GLG_Test.sh $runtime $data -outdir $outdir $arg
 done < $hypefile
 
-./run_SINGE_Aggregate.sh /usr/local/MATLAB/MATLAB_Runtime/v94 $gene_list $data $outdir
+./run_SINGE_Aggregate.sh $runtime $gene_list $data $outdir
