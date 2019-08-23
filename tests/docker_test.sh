a2aeb8c16154adce146316f1ada5e83427362f7f
Anthony Gitter <gitter@biostat.wisc.edu>
2019-08-23 05:54:13 -0500

    Test output from compiled SINGE_Example

--- tests/docker_test.sh
+++ tests/docker_test.sh
@@ -5,7 +5,7 @@ set -o errexit
 # Move the binaries and check the versions of the source and binaries (md5sums)
 mv /download/* .
 # Append the binary md5sum to the md5sums of the tracked source code files
-md5sum SINGE_GLG_Test SINGE_Aggregate >> current_code.md5
+md5sum SINGE_Example SINGE_GLG_Test SINGE_Aggregate >> current_code.md5
 cat current_code.md5
 # An error similar to:
 # 'code.md5 current_code.md5 differ: char 174, line 4'
@@ -13,12 +13,20 @@ cat current_code.md5
 # the expected versions
 cmp code.md5 current_code.md5
 
-# Run SINGE on the example data and inspect the output
-./standalone_SINGE.sh data1/X_SCODE_data.mat data1/tf.mat Output tests/example_hyperparameters.txt
-ls Output/ -l
-
 # Confirm the contents of the conda environment
 conda list
 
-# Run the tests to compare the SINGE outputs
-tests/compare_example_output.sh
+# Run SINGE on the example data using the bash script and inspect the output
+./standalone_SINGE.sh data1/X_SCODE_data.mat data1/tf.mat script_output tests/example_hyperparameters.txt /usr/local/MATLAB/MATLAB_Runtime/v94
+ls script_output/ -l
+
+# Run the tests to compare the SINGE outputs from the standalone script
+tests/compare_example_output.sh script_output
+
+# Run SINGE on the example data using the compiled SINGE_Example
+./run_SCINGE_Example.sh /usr/local/MATLAB/MATLAB_Runtime/v94
+ls Output/ -l
+
+# Run the tests to compare the SINGE outputs from the compiled SINGE_Example
+# The output directory is hard-coded to Output
+tests/compare_example_output.sh Output
