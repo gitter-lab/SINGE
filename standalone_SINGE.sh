5b594f08f94f2b570259411b9f4e3b60d77a24e3
Anthony Gitter <gitter@biostat.wisc.edu>
2019-09-14 07:44:36 -0500

    Update argument syntax

--- standalone_SINGE.sh
+++ standalone_SINGE.sh
@@ -5,7 +5,7 @@ outdir=$3
 hypefile=$4
 runtime=$5
 while read arg||[ -n "$arg" ]; do 
-    ./run_SINGE_GLG_Test.sh $runtime $data -outdir $outdir $arg
+    ./run_SINGE_GLG_Test.sh $runtime $data --outdir $outdir $arg
 done < $hypefile
 
 ./run_SINGE_Aggregate.sh $runtime $gene_list $data $outdir
