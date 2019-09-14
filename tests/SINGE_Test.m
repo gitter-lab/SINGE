35dd01046f1a3d658100e94cae670334578fc94b
Anthony Gitter <gitter@biostat.wisc.edu>
2019-09-14 07:21:04 -0500

    Update relative paths

--- tests/SINGE_Test.m
+++ tests/SINGE_Test.m
@@ -8,10 +8,10 @@ if ~isdeployed
 end
 
 %% Specify path to input data and path to output directory, gene list, and hyperparameter file
-data = '../data1/X_SCODE_data.mat';
-outdir = '../compiled_output';
-gene_list = '../data1/tf.mat';
-hyperparameter_file = 'example_hyperparameters.txt';
+data = 'data1/X_SCODE_data.mat';
+outdir = 'compiled_output';
+gene_list = 'data1/tf.mat';
+hyperparameter_file = 'tests/example_hyperparameters.txt';
 
 %% Run SINGE
 SINGE(data,gene_list,outdir,hyperparameter_file);
