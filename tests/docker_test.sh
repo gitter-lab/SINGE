76140b19ccb28b2b60eb59ed90180f86ef5b4ef6
Anthony Gitter <gitter@biostat.wisc.edu>
2019-11-11 22:49:10 -0600

    Use new file locations for md5sum comparisons

--- tests/docker_test.sh
+++ tests/docker_test.sh
@@ -4,13 +4,13 @@ set -o errexit
 
 # Check the versions of the source and binaries (md5sums)
 # Append the binary md5sum to the md5sums of the tracked source code files
-md5sum tests/SINGE_Test SINGE_GLG_Test SINGE_Aggregate >> current_code.md5
+md5sum $SINGE_ROOT/tests/SINGE_Test $SINGE_ROOT/SINGE_GLG_Test $SINGE_ROOT/SINGE_Aggregate >> current_code.md5
 cat current_code.md5
 # An error similar to:
 # 'code.md5 current_code.md5 differ: char 174, line 4'
 # indicates the md5sums of the current source and binary files do not match
 # the expected versions
-cmp code.md5 current_code.md5
+cmp $SINGE_ROOT/code.md5 current_code.md5
 
 # Confirm the contents of the conda environment
 conda list
