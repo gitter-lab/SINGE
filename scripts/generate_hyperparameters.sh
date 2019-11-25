76ff9f4f3ad6b9b5c31ac263dee0d48cd96c5a29
Atul <atulpdeshpande@gmail.com>
2019-11-24 18:26:43 -0600

    Uniform input format

--- scripts/generate_hyperparameters.sh
+++ scripts/generate_hyperparameters.sh
@@ -10,14 +10,19 @@ listname="hyperparameters_$DATEname.txt"
 rm $listname
 touch $listname
 ID=0
+lambda=[$(
+while read lam
+do
+  echo -n "$lam,"
+done < lambda.txt)
+lambda=${lambda::-1}]
+echo $lambda
 for replicate in $(seq 1 $numreplicates)
 do
-	while read lambda; do
-		while read dT num_lags; do
+	while read dT num_lags; do
 			while IFS='' read -r kernel; do
 				echo "--lambda $lambda --dT $dT --num-lags $num_lags --kernel-width $kernel --ID $ID --replicate $replicate --family $family --date $DATE --prob-zero-removal $pzr --prob-remove-sample $prs">>$listname
 				ID=$((ID+1))
 			done < kernel.txt
 		done < time.txt 
-	done < lambda.txt 
 done
