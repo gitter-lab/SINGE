1b8029dc7580d1d59062d4347197eaa4163cff20
Atul <atul@Atuls-MacBook-Pro.local>
2020-07-31 15:57:21 -0400

    enable branching processes

--- code/SINGE_GLG_Test.m
+++ code/SINGE_GLG_Test.m
@@ -9,18 +9,21 @@ tic
 params = parseParams(Data,varargin{:})
 mkdir(params.outdir)
 %%% End InputParser
-tic;
 if isdeployed
     set(0,'DefaultFigureVisible','off');
 end
-resampling_method = {'holes';'burst'};
-
 
 % Use a temporary matfile to track intermediate state
 % Create new file for each job ID to avoid bugs when storage is shared
 % between parallel jobs
 m = matfile(['TempMat' '_' num2str(params.ID)],'Writable',true);
 
+if ismember('branches',who(m))
+    n_branches = size(m.branches,2);
+else 
+    n_branches = 1;
+end
+params.n_branches = n_branches;
 ptime = m.ptime;
 m.computeKp = 1;
 if ptime(end)~=100
@@ -30,20 +33,29 @@ end
 [LX,WX] = size(m,'X');
 params.LX = LX;
 params.WX = WX;
+
 % Even if an index of regulators is specified in the input file
 % we create a square adjacency matrix
 Adj_Matrix = sparse(zeros(LX));
 % To improve efficiency, we perform GLG tests for all lambda values at once
 % (uses glmnet's warm start functionality). The following lines create
 % multiple filenames for storing each GLG output.
-for ii = 1:length(params.lambda)
-    filename{ii} = ['AdjMatrix_' params.Data];
-    filename{ii} = [filename{ii} '_ID_' num2str(params.ID) '_lambda_' num2str(params.lambda(ii)) '_replicate_' num2str(params.replicate)];
-    filename{ii}(filename{ii}=='.') = 'p';
-    filename{ii}(filename{ii}=='/') = '_';
-    filename{ii} = fullfile(params.outdir,filename{ii});
-    save(filename{ii},'Adj_Matrix','varargin','params','-v7.3');
-    outs{ii} = matfile(filename{ii},'Writable',true);
+n_lambda = length(params.lambda);
+for jj = 1:n_branches
+    for ii = 1:n_lambda
+        kk = (jj-1)*n_lambda+ii;
+        filename{kk} = ['AdjMatrix_' params.Data];
+        if n_branches>1
+            filename{kk} = [filename{kk} '_ID_' num2str(params.ID) 'p' num2str(jj) '_lambda_' num2str(params.lambda(ii)) '_replicate_' num2str(params.replicate)];
+        else
+            filename{kk} = [filename{kk} '_ID_' num2str(params.ID) '_lambda_' num2str(params.lambda(ii)) '_replicate_' num2str(params.replicate)];
+        end
+        filename{kk}(filename{kk}=='.') = 'p';
+        filename{kk}(filename{kk}=='/') = '_';
+        filename{kk} = fullfile(params.outdir,filename{kk});
+        save(filename{kk},'Adj_Matrix','varargin','params','-v7.3');
+        outs{kk} = matfile(filename{kk},'Writable',true);
+    end
 end
 randomizer = floor(params.DateNumber+sum(params.lambda)*1000+params.dT+params.p1+params.kernel_width*10+params.replicate)
 %rng('default');
@@ -58,17 +70,18 @@ if params.replicate>0
 end
 
 lastprogress = 0;
-for irow = 1:1:LX
-    [for_metric] = run_iLasso_row(m,outs,params,irow);
-    runtime = toc;
-    progress = (irow)/LX*100;
-    if (progress-lastprogress)>=10
-        s = sprintf(['%2.5g %% Progress in %5.5g seconds'],progress,runtime);
-        disp(s);
-        lastprogress = progress;
-	end
+for jj = 1:n_branches
+    for irow = 1:1:LX
+        [for_metric] = run_iLasso_row(m,outs,params,irow);
+        runtime = toc;
+        progress = (irow)/LX*100;
+        if (progress-lastprogress)>=10
+            s = sprintf(['%2.5g %% Progress in %5.5g seconds'],progress,runtime);
+            disp(s);
+            lastprogress = progress;
+        end
+    end
 end
-
 runtime = toc
 
 % File saving moved to iLasso_for_SINGE using matfile feature
